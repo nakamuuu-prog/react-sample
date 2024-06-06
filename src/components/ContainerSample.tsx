c79cf2b0d3d4b6db9149c905f8eb285154f65e27
nakamuuu-prog <affilimoney2@gmail.com>
2024-06-07 00:26:19 +0900

    propsの書き換えと子要素を増やしたとき

--- src/components/ContainerSample.tsx
+++ src/components/ContainerSample.tsx
@@ -1,7 +1,17 @@
 // 親からタイトルと子要素を受け取る
 // 親から子へ渡すpropsは一方通行。子からpropsの中身を書き換えることはできない。
-const Container = (props: { title: string; children: React.ReactElement }) => {
+const Container = (props: {
+  title: string;
+  children: React.ReactElement;
+  children2: React.ReactElement;
+}) => {
   const { title, children } = props;
+
+  // エラーにならない。どうゆうこと？
+  // props = { title: "あいうえお", children: <p>かきくけこ</p> };
+  // propsの中身を直接書き換えようとするとエラーになる
+  // 定数であるため、'title' に代入することはできません。ts(2588)
+  // title = "さしすせそ";
 
   return (
     <div style={{ background: "red" }}>
@@ -13,10 +23,13 @@ const Container = (props: { title: string; children: React.ReactElement }) => {
 
 const Perent = () => {
   return (
-    // titleはMessageコンポーネントと同じよう渡す
+    // React.ReactElement引数を増やして要素追加でいけると思ったけどダメ。脱線するから一旦置いとく。
+    // この JSX タグの 'children' prop は型 'ReactElement<any, string | JSXElementConstructor<any>>' の単一の子を予期しますが、複数の子が指定されました。ts(2746)
     <Container title="Hello">
       {/* Containerコンポーネント内の子要素がchildrenとして渡される */}
+      {/* 2つ渡す時はどうするの？ */}
       <p>ここの部分が背景色で囲まれます</p>
+      <p>この要素は？</p>
     </Container>
   );
 };
