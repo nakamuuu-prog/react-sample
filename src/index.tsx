d8101f3434b36e1e727dda384b137ea8e12388c3
nakamuuu-prog <affilimoney2@gmail.com>
2024-06-06 22:19:10 +0900

    3.1.2 Reactの基本 - Reactの基本のキーワード

--- src/index.tsx
+++ src/index.tsx
@@ -9,13 +9,8 @@ const root = ReactDOM.createRoot(
   // index.htmlにあるid属性
   document.getElementById("root") as HTMLElement
 );
-root.render(
-  // 不適切なコードを検知するためのヘルパー
-  <React.StrictMode>
-    {/* 取り込んだAppコンポーネントをJSXタグとして使用できる */}
-    <App />
-  </React.StrictMode>
-);
+// 直接HTMLタグをrenderに渡して描写される
+root.render(<h1>見出し</h1>);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
